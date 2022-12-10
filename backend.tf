69c796af8cb9d1d592cdf832444f7751548e644c
AkshayPatil5995 <119591862+AkshayPatil5995@users.noreply.github.com>
2022-12-10 18:29:48 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "s3" {
         bucket = "tf-terraform-demo123"
-        key = "devopstfdem1"
+        key = "devopstfdem11"
         region = "ap-south-1"
         dynamodb_table = "akshay"
       
